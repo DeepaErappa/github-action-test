e620149fa8127083feee6a4fb70dbc48cd738e27
Deepa Erappa <deepa_erappa@thbs.india.com>
2025-08-01 17:50:26 +0530

    update logic

--- main.py
+++ main.py
@@ -42,6 +42,7 @@ def replace_properties(env_yaml_file):
         raise KeyError(f"'properties' not found in {env_yaml_file}")
 
     api_data['properties'] = env_data['properties']
+    
     write_yaml(API_YAML, api_data)
     print(f"[✓] Updated 'properties' in {API_YAML} using {env_yaml_file}")
 
