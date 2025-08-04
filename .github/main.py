71662f76df2b240fd2567cc0995f82a74c862eb4
Abhirup Bhattacharjee <abhirup.bhattacharjee1@virginmediao2.co.uk>
2025-08-04 11:21:54 +0530

    Update main.py

--- .github/main.py
+++ .github/main.py
@@ -7,7 +7,7 @@ import requests
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Go one level up to reach repo root
 REPO_ROOT = os.path.abspath(os.path.join(SCRIPT_DIR, ".."))
-EXTERNAL_REPO = "DeepaErappa/git-config-files"  # e.g., "DeepaErappa/config-repo"
+EXTERNAL_REPO = "abhirup-vmo2/tmf-config-repo-apic"  # e.g., "DeepaErappa/config-repo"
 EXTERNAL_BRANCH = "main"  # or whatever branch holds the staging/prod.yaml
 
 # Absolute paths to API and config directories
